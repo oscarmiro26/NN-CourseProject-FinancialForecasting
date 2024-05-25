5b15867dc122aff6e76876f28bc5c24781b1f909
Mathias Randruut <mathias.randruut@gmail.com>
2024-05-25 19:33:58 +0200

    mlp model start

---
+++ models/mlp_model.py
@@ -0,0 +1,20 @@
+# models/mlp_model.py
+
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+def get_mlp_model(input_size=1, hidden_size=10, output_size=1):
+    return MLP(input_size, hidden_size, output_size)
