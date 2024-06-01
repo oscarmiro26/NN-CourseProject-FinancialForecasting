976753bffa20ed298db9fd80870b0ac4768a80bd
Mathias Randruut <mathias.randruut@gmail.com>
2024-06-01 15:54:09 +0200

    fix

--- save_file.py
+++ save_file.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import os
 
-file_path = '~/Desktop/NeuralNetworks/NNProject/'
+file_path = '/Users/mathiasrandruut/neuralnetcode/NN-CourseProject-FinancialForecasting'
 file_name = 'M3C.xls'
 sheet = 'M3Month'
 
 df = pd.read_excel(os.path.join(file_path, file_name), sheet_name=sheet)
 
-csv_file = 'M3C_Monthly.csv'
-df.to_csv(os.path.join(file_path, csv_file), index=False)
+df['Category'] = df['Category'].str.strip()
+
+df_filtered = df[df['Category'] == 'FINANCE']
+
+csv_file_filtered = 'M3C_Monthly_FINANCE.csv'
+
+df_filtered.to_csv(os.path.join(file_path, csv_file_filtered), index=False)
